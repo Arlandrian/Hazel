83de028da8d8dbf395ab7b526d18030b7b00d979
Arlandrian <alo_istanbul@hotmail.com>
2019-06-27 16:37:21 +0300

    Renderer Abstraction

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -2,9 +2,11 @@
 #include "Application.h"
 
 //#include "GLFW/glfw3.h"
-#include <glad/glad.h>
+//#include <glad/glad.h>
 
 #include "Hazel/Input.h"
+#include "Hazel/Renderer/Renderer.h"
+
 #include "glm/glm.hpp"
 
 namespace Hazel {
@@ -182,25 +184,23 @@ namespace Hazel {
 	void Application::Run() {
 
 		while( m_Running ) {
-			glClearColor(0.1f, 0.1f, 0.1f, 1);
-			glClear(GL_COLOR_BUFFER_BIT);
+
+			RenderCommand::SetClearColor({0.1f, 0.1f, 0.1f, 1});
+			RenderCommand::Clear();
 
 			//Renderer::BeginScene(camera,lights,environment);
-			/*
+			
 			Renderer::BeginScene();
 
-			Renderer::Submit();
-			
-			Renderer::EndScene();
-			*/
 			m_BlueShader->Bind();
-			m_SqrVertexArray->Bind();
-			glDrawElements(GL_TRIANGLES, m_SqrVertexArray->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
+			Renderer::Submit(m_SqrVertexArray);
 
 			m_Shader->Bind();
-			m_VertexArray->Bind();
-			glDrawElements(GL_TRIANGLES, m_VertexArray->GetIndexBuffer()->GetCount() , GL_UNSIGNED_INT, nullptr);
+			Renderer::Submit(m_VertexArray);
 
+			
+			Renderer::EndScene();
+			
 			
 
 			for( Layer* layer : m_LayerStack ) {
