d3e5777cb0e2ba31748d59b88759611bee3c3cb3
Arlandrian <alo_istanbul@hotmail.com>
2019-10-31 21:03:14 +0300

    clean up

--- SandBox/src/Sandbox2D.cpp
+++ SandBox/src/Sandbox2D.cpp
@@ -16,33 +16,6 @@ Sandbox2D::Sandbox2D()
 void Sandbox2D::OnAttach() {
 
 
-	m_SquareVA = Hazel::VertexArray::Create();
-
-	float squareVertices[3 * 4] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.5f,  0.5f, 0.0f,
-		-0.5f,  0.5f, 0.0f
-	};
-
-	Hazel::Ref<Hazel::VertexBuffer> squareVB;
-	squareVB = Hazel::VertexBuffer::Create(squareVertices, sizeof(squareVertices));
-
-	Hazel::BufferLayout sqrBufferLayut = {
-		{ Hazel::ShaderDataType::Float3, "a_Position" }
-	};
-	squareVB->SetLayout(sqrBufferLayut);
-	m_SquareVA->AddVertexBuffer(squareVB);
-
-	// Index Buffer
-	uint32_t squareIndices[3 * 2] = {
-		0,1,2,2,3,0
-	};
-	Hazel::Ref<Hazel::IndexBuffer> squareIB;
-	squareIB = Hazel::IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t));
-	m_SquareVA->SetIndexBuffer(squareIB);
-
-	m_FlatColorShader = Hazel::Shader::Create("assets/shaders/FlatColor.glsl");
 }
 
 void Sandbox2D::OnDetach() {
@@ -61,29 +34,7 @@ void Sandbox2D::OnUpdate(Hazel::Timestep ts) {
 	Hazel::Renderer2D::DrawQuad({ 0.0f,0.0f }, { 1.0f,1.0f }, { 0.8f, 0.3f, 0.2f, 1.0f });
 
 	Hazel::Renderer2D::EndScene();
-	/*
-	glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
-	// TODO: Add these functions - Shader::SetMat4(), Shader::SetFloat4();
-	auto shader = std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_FlatColorShader);
-	shader->Bind();
-	shader->UploadUniformFloat4("u_Color", m_SquareColor);
-
-
-	for (int y = -10; y < 10; y++) {
-
-		for (int x = -10; x < 10; x++) {
-			glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
-
-			glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-
-			Hazel::Renderer::Submit(shader, m_SquareVA, transform);
-		}
-	}
-
-
-	Hazel::Renderer2D::EndScene();
-	*/
 }
 
 void Sandbox2D::OnImGuiRender() {
